ea491a6045227f340a4c06e2f2d4773284173946
Karina <karinamatles@gmail.com>
2017-06-02 19:35:39 -0300

    Metodo setNombre
    Se le agregó el atributo nombre al equipo y su correspondiente método
    set

--- src/modelo/juego/Equipo.java
+++ src/modelo/juego/Equipo.java
@@ -7,9 +7,14 @@ import modelo.personajes.Personaje;
 public class Equipo {
 	
 	private List<Personaje> personajes;
+	private String nombre;
 	
 	public Equipo(List<Personaje> personajes){
 		this.personajes = personajes;
+	}
+	
+	public void setNombre(String nombre){
+		this.nombre = nombre;
 	}
 
 	public boolean pertenece(Personaje personaje) {
