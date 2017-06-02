df4cbac5717d912e3d56456b6aa24bca927bf725
Karina <karinamatles@gmail.com>
2017-06-02 19:39:29 -0300

    Nuevas clases
    Clases Jugador, Tablero y TestDragonBall

---
+++ src/modelo/tablero/Tablero.java
@@ -0,0 +1,10 @@
+package modelo.tablero;
+
+public class Tablero {
+
+	public static Tablero crearNuevo(int size) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/pruebas/TestDragonBall.java
@@ -0,0 +1,19 @@
+package pruebas;
+
+import org.junit.Assert;
+import org.junit.Before;
+
+import modelo.juego.DragonBall;
+
+
+
+public class TestDragonBall {
+	private DragonBall dragonBall;
+	
+	@Before
+	public void setUp(){
+		dragonBall = new DragonBall();
+	}
+
+	
+}
