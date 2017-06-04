c540b1c7a390a95d5299881c400c7a43ac4c27ac
Emanuel <emanuel.pelozo@outlook.com>
2017-06-03 21:56:30 -0300

    test Tablero

--- src/pruebas/TestTablero.java
+++ src/pruebas/TestTablero.java
@@ -7,6 +7,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import modelo.excepciones.PosicionFueraDeRango;
+import modelo.personajes.Goku;
+import modelo.personajes.Personaje;
 import modelo.tablero.Casillero;
 import modelo.tablero.Posicion;
 import modelo.tablero.Tablero;
@@ -20,8 +22,27 @@ public class TestTablero {
 		sizeTablero = 6;
 		tablero = new Tablero(6);
 		
+		
 	}
-
+	
+	@Test
+	public void alCrearTableroTodosLosCasillerosEstanVacios(){
+		Posicion pos;
+		Casillero casillero;
+		boolean estaVacio = true;
+		
+		for(int x=0 ; x < this.sizeTablero ; x++){
+			for(int y=0 ; y < this.sizeTablero ; y++){
+				pos= new Posicion(x,y);
+				casillero = this.tablero.getCasillero(pos);
+				 if(!casillero.estaVacio()){
+					 estaVacio = false;
+				 }
+			}
+		}
+		Assert.assertTrue(estaVacio);
+	}
+	
 	@Test
 	public void getCasilleroSegunPosicionDevuelveElCasilleroConLaMismaPosicion() throws PosicionFueraDeRango {
 		Posicion posBuscada = new Posicion(2,2);
@@ -30,6 +51,43 @@ public class TestTablero {
 		
 		Assert.assertTrue( posBuscada.equals(posCasillero) );	
 	}
+	
+	@Test
+	public void getCasilleroDeUnaPosicionFueraDeRangoLevantaExcepcion(){
+		Posicion pos = new Posicion(-2,-2);
+		try{
+			this.tablero.getCasillero(pos);
+			Assert.fail("Deberia haber levantado la excepcion");
+		}
+		catch(PosicionFueraDeRango e){
+			Assert.assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void alPosicionarPersonajeEnTableroElCasilleroNoEstaVacio(){
+		Personaje personaje = new Goku(this.tablero);
+		Posicion pos = new Posicion(1,1);
+		Casillero casillero = this.tablero.getCasillero(pos);
+		this.tablero.posicionarPersonaje(personaje, pos);
+		
+		Assert.assertTrue( !casillero.estaVacio() );	
+	}
+	
+	@Test
+	public void alReposicionarPersonajeLosElNuevoCasilleroNoEstaVacio(){
+		Personaje personaje = new Goku(this.tablero);
+		Posicion posInicial = new Posicion(0,0);
+		Posicion posFinal = new Posicion (5,5);
+		this.tablero.posicionarPersonaje(personaje, posInicial);
+		
+		this.tablero.reposicionarPersonaje(personaje, posFinal);
+		Assert.assertTrue( (this.tablero.getCasillero(posInicial)).estaVacio() );
+		Assert.assertTrue( !(this.tablero.getCasillero(posFinal)).estaVacio() );
+		
+	
+	}
+	
 	
 	
 }
