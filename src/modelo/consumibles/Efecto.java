e412efc47a9ee2033ca897e7894434ecceb60b65
Emanuel <emanuel.pelozo@outlook.com>
2017-06-07 09:45:16 -0300

    agregada clase Efecto

---
+++ src/modelo/consumibles/Efecto.java
@@ -0,0 +1,27 @@
+package modelo.consumibles;
+
+import modelo.personajes.Personaje;
+
+public abstract class Efecto {
+	private int duracion;
+	private int costoKi;
+	
+	public Efecto (int duracion, int costoKi){
+		this.duracion = duracion;
+		this.costoKi = costoKi;
+	}
+	public void regenerarVida(Personaje personaje){
+		return;
+	}
+	public void modificarVelocidad(Personaje personaje){
+		return;
+	}
+	public void modificarPoderPelea(Personaje personaje){
+		return;
+	}
+	public boolean permiteTransformacion(){ //para inutilizar
+		return true;
+	}
+
+
+}
