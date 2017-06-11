2572af3174676f4325eaa4f8106a382c7a9f3cd7
Emanuel <emanuel.pelozo@outlook.com>
2017-06-11 13:02:42 -0300

    borre la libreria javafx que agregue

--- src/vista/MenuJuego.java
+++ src/vista/MenuJuego.java
@@ -1,7 +1,14 @@
 package vista;
 import javafx.application.Application;
+import javafx.stage.Stage;
 
 
 public class MenuJuego extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
