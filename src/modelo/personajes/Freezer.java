ac9a6d74b96beea72f17b2e407c4d9edb0256b3c
blobosco <bdlobosco93@gmail.com>
2017-06-14 01:18:23 -0300

    Agrego Const Freezer y MajinBoo

--- src/modelo/personajes/Freezer.java
+++ src/modelo/personajes/Freezer.java
@@ -7,21 +7,31 @@ import modelo.personajes.modos.PrimeraTransformacion;
 import modelo.personajes.modos.SegundaTransformacion;
 
 public class Freezer extends Personaje{
+	
+	public static final int vidaInicial = 400;
+	public static final int poderPeleaNormal = 20;
+	public static final int poderPeleaSegundaForma = 40;
+	public static final int poderPeleaDefinitivo = 50;
+	public static final int distanciaAtaqueNormal=2;
+	public static final int distanciaAtaqueTranformado=3;
+	public static final int velocidadNormal = 4;
+	public static final int velocidadPrimerTranformacion = 4;
+	public static final int velocidadSegundaTranformacion = 6;
 
 	public Freezer(Tablero tablero) {
-		super(400, new ModoNormal(20,2,4, "Freezer"), new AtaquePotenciador(20,1.5), tablero, 20, 50);
+		super(vidaInicial, new ModoNormal(poderPeleaNormal,distanciaAtaqueNormal,velocidadNormal, "Freezer"), new AtaquePotenciador(20,1.5), tablero, 20, 50);
 	}
 	
 	@Override
 	public Modo realizarPrimeraTransformacion(){
 		super.restarKiPrimeraTransformacion();
-		return new PrimeraTransformacion(40,3,4, "Freezer Segunda Forma");
+		return new PrimeraTransformacion(poderPeleaSegundaForma,distanciaAtaqueTranformado,velocidadPrimerTranformacion, "Freezer Segunda Forma");
 	}
 	
 	@Override
 	public Modo realizarSegundaTransformacion() {
 		super.restarKiSegundaTransformacion();
-		return new SegundaTransformacion(50,3,6, "Freezer Definitivo");
+		return new SegundaTransformacion(poderPeleaDefinitivo,distanciaAtaqueTranformado,velocidadSegundaTranformacion, "Freezer Definitivo");
 	}
 
 }
--- src/modelo/personajes/MajinBoo.java
+++ src/modelo/personajes/MajinBoo.java
@@ -7,21 +7,32 @@ import modelo.personajes.modos.PrimeraTransformacion;
 import modelo.personajes.modos.SegundaTransformacion;
 
 public class MajinBoo extends Personaje {
+	
+	public static final int vidaInicial = 300;
+	public static final int poderPeleaNormal = 30;
+	public static final int poderPeleaBooMalo = 50;
+	public static final int poderPeleaBooOriginal = 60;
+	public static final int distanciaAtaqueNormal=2;
+	public static final int distanciaAtaqueBooMalo=2;
+	public static final int distanciaAtaqueBooOriginal=3;
+	public static final int velocidadNormal = 2;
+	public static final int velocidadPrimerTranformacion = 3;
+	public static final int velocidadSegundaTranformacion = 4;
 
 	public MajinBoo(Tablero tablero) {
-		super(300, new ModoNormal(30,2,2, "Majin Boo"), new AtaqueInmovilizador(30), tablero, 20, 50);
+		super(vidaInicial, new ModoNormal(poderPeleaNormal,distanciaAtaqueNormal,velocidadNormal, "Majin Boo"), new AtaqueInmovilizador(30), tablero, 20, 50);
 	}
 
 	@Override
 	public Modo realizarPrimeraTransformacion(){
 		super.restarKiPrimeraTransformacion();
-		return new PrimeraTransformacion(50,2,3, "Boo Malo");
+		return new PrimeraTransformacion(poderPeleaBooMalo,distanciaAtaqueBooMalo,velocidadPrimerTranformacion, "Boo Malo");
 	}
 	
 	@Override
 	public Modo realizarSegundaTransformacion() {
 		super.restarKiSegundaTransformacion();
-		return new SegundaTransformacion(60,3,4, "Boo Original");
+		return new SegundaTransformacion(poderPeleaBooOriginal,distanciaAtaqueBooOriginal,velocidadSegundaTranformacion, "Boo Original");
 	}
 
 	
