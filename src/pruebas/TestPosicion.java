d9932d8c1a4853fd5de56214a3885c7bbc6d213a
Mati <Mati@192.168.0.23>
2017-06-16 19:20:44 -0300

    refactorizacion tests posicion

--- src/pruebas/TestPosicion.java
+++ src/pruebas/TestPosicion.java
@@ -9,6 +9,7 @@ import modelo.juego.Posicion;
 public class TestPosicion {
 
 	private Posicion posicion;
+	private int sizeTableroPrueba = 10;
 	
 	@Before
 	public void setUp() {
@@ -22,44 +23,51 @@ public class TestPosicion {
 	
 	@Test
 	public void testCalcularDistanciaHorizontalAdelante(){
+		int distanciaEsperada = 3;
 		Posicion destino = new Posicion (8,5);
-		Assert.assertEquals(3, posicion.distanciaA(destino));
+		Assert.assertEquals(distanciaEsperada, posicion.distanciaA(destino));
 	}
 	
 	@Test
 	public void testCalcularDistanciaHorizontalAtras(){
+		int distanciaEsperada = 4;
 		Posicion destino = new Posicion (1,5);
-		Assert.assertEquals(4, posicion.distanciaA(destino));
+		Assert.assertEquals(distanciaEsperada, posicion.distanciaA(destino));
 	}
 	
 	@Test
 	public void testCalcularDistanciaVerticalAdelante(){
+		int distanciaEsperada = 1;
 		Posicion destino = new Posicion (5,6);
-		Assert.assertEquals(1, posicion.distanciaA(destino));
+		Assert.assertEquals(distanciaEsperada, posicion.distanciaA(destino));
 	}
 	
 	@Test
 	public void testCalcularDistanciaVerticalAtras(){
+		int distanciaEsperada = 2;
 		Posicion destino = new Posicion (5,3);
-		Assert.assertEquals(2, posicion.distanciaA(destino));
+		Assert.assertEquals(distanciaEsperada, posicion.distanciaA(destino));
 	}
 	
 	@Test
 	public void testCalcularDistanciaDiagonal(){
+		int distanciaEsperada = 2;
 		Posicion destino = new Posicion (7,7);
-		Assert.assertEquals(2, posicion.distanciaA(destino));
+		Assert.assertEquals(distanciaEsperada, posicion.distanciaA(destino));
 	}
 	
 	@Test
 	public void testCalcularDistanciaAOtraPosicion(){
+		int distanciaEsperada = 3;
 		Posicion destino = new Posicion (4,8);
-		Assert.assertEquals(3, posicion.distanciaA(destino));
+		Assert.assertEquals(distanciaEsperada, posicion.distanciaA(destino));
 	}
 	
 	@Test
 	public void testSumarMismaPosicion(){
+		Posicion posicionEsperada = new Posicion (10,10);
 		Posicion resultado = posicion.sumarPosicion(posicion);
-		Assert.assertTrue(resultado.equals(new Posicion (10,10)));
+		Assert.assertTrue(resultado.equals(posicionEsperada));
 	}
 	
 	@Test
@@ -70,31 +78,32 @@ public class TestPosicion {
 	
 	@Test
 	public void testSumarCualquierPosicion(){
+		Posicion posicionEsperada = new Posicion (12,7);
 		Posicion resultado = posicion.sumarPosicion(new Posicion(7,2));
-		Assert.assertTrue(resultado.equals(new Posicion (12,7)));
+		Assert.assertTrue(resultado.equals(posicionEsperada));
 	}
 	
 	@Test
 	public void testPosicionXNegativoNoEsValida(){
 		this.posicion = new Posicion (-1,4);
-		Assert.assertFalse(posicion.esValida(10));
+		Assert.assertFalse(posicion.esValida(sizeTableroPrueba));
 	}
 	
 	@Test
 	public void testPosicionYNegativoNoEsValida(){
 		this.posicion = new Posicion (3,-5);
-		Assert.assertFalse(posicion.esValida(10));
+		Assert.assertFalse(posicion.esValida(sizeTableroPrueba));
 	}
 	
 	@Test
 	public void testPosicionXMuyAltoNoEsValida(){
 		this.posicion = new Posicion (20,3);
-		Assert.assertFalse(posicion.esValida(10));
+		Assert.assertFalse(posicion.esValida(sizeTableroPrueba));
 	}
 	
 	@Test
 	public void testPosicionYMuyAltoNoEsValida(){
 		this.posicion = new Posicion (5,18);
-		Assert.assertFalse(posicion.esValida(10));
+		Assert.assertFalse(posicion.esValida(sizeTableroPrueba));
 	}
 }
